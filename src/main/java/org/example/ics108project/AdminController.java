56efa1af95f0949fae6f0c66c38800e467ee1e72
Almaan <almaan.khan@outlook.com>
2024-04-05 15:39:36 +0300

    test commit 2

--- src/main/java/org/example/ics108project/AdminController.java
+++
@@ -1,4 +0,0 @@
-package org.example.ics108project;
-
-public class AdminController {
-}
--- src/main/java/org/example/ics108project/SceneController.java
+++ src/main/java/org/example/ics108project/SceneController.java
@@ -1,4 +1,4 @@
 package org.example.ics108project;
 
-public class UserController {
+public class SceneController {
 }
