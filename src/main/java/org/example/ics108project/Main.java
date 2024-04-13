28e376231636028d525c4157a42edbb5894aafa3
Almaan Khan <110341624+A1maan@users.noreply.github.com>
2024-04-13 16:32:33 +0300

    testing

--- src/main/java/org/example/ics108project/Main.java
+++ src/main/java/org/example/ics108project/Main.java
@@ -2,19 +2,25 @@ package org.example.ics108project;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
 
 public class Main extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage stage){
+        try{
+            System.out.println(getClass().getResource("Admin.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("Admin.fxml"));
+            Scene scene = new Scene(root);
+            stage.setTitle("Hello!");
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/org/example/ics108project/SceneController.java
+++ src/main/java/org/example/ics108project/SceneController.java
@@ -1,7 +1,11 @@
 package org.example.ics108project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class SceneController {
+    public void AddEvent(ActionEvent e){
+        System.out.println("test");
+    }
 }
